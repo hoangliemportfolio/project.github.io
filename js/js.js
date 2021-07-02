6e65dfa5540761bcbc04a0394165c8fccc66aa86
hoangliem1997 <liemph.1997@gmail.com>
2021-07-02 17:09:05 +0700

    f

--- js/js.js
+++ js/js.js
@@ -1,3 +1,12 @@
+// // Nhạc nền
+// $('document').ready(function() {
+//     document.addEventListener('mouseover', function()
+//     {
+//         var audio = document.querySelector('.bgSound');
+//         audio.play()
+//     })
+//     });
+
 
 
 // Phần thay đổi hình nền của trang chủ --------------------
@@ -30,11 +39,18 @@ slide();
 
 
 // Phần lật bài --------------------
-
+var flipSound1 = document.querySelector('.flipSound1')
+var flipSound2 = document.querySelector('.flipSound2')
+var flipSound3 = document.querySelector('.flipSound3')
+var flipSound4 = document.querySelector('.flipSound4')
+var flipSound5 = document.querySelector('.flipSound5')
 var card1 = document.getElementsByClassName('flip1')
 function flip1() {
     for ( var i = 0; i < card1.length; i++ ) {
-        card1[i].classList.remove('flip1')
+        card1[i].classList.remove('flip1');
+        flipSound1.pause();
+        flipSound1.currentTime = 0;
+        flipSound1.play()
     }
     setTimeout(flip1, 8000)
     }
@@ -45,6 +61,9 @@ var card2 = document.getElementsByClassName('flip2')
 function flip2() {
     for ( var i = 0; i < card2.length; i++ ) {
         card2[i].classList.remove('flip2')
+        flipSound2.pause();
+        flipSound2.currentTime = 0;
+        flipSound2.play()
     }
     setTimeout(flip2, 7500)
     }
@@ -55,6 +74,9 @@ var card2 = document.getElementsByClassName('flip2')
 function flip2() {
     for ( var i = 0; i < card2.length; i++ ) {
         card2[i].classList.remove('flip2')
+        flipSound2.pause();
+        flipSound2.currentTime = 0;
+        flipSound2.play()
     }
     setTimeout(flip2, 7900)
     }
@@ -66,6 +88,9 @@ var card3 = document.getElementsByClassName('flip3')
 function flip3() {
     for ( var i = 0; i < card3.length; i++ ) {
         card3[i].classList.remove('flip3')
+        flipSound3.pause();
+        flipSound3.currentTime = 0;
+        flipSound3.play()
     }
     setTimeout(flip3, 6000)
     }
@@ -77,6 +102,9 @@ var card4 = document.getElementsByClassName('flip4')
 function flip4() {
     for ( var i = 0; i < card4.length; i++ ) {
         card4[i].classList.remove('flip4')
+        flipSound4.pause();
+        flipSound4.currentTime = 0;
+        flipSound4.play()
     }
     setTimeout(flip4, 7000)
     }
@@ -87,6 +115,9 @@ var card5 = document.getElementsByClassName('flip5')
 function flip5() {
     for ( var i = 0; i < card5.length; i++ ) {
         card5[i].classList.remove('flip5')
+        flipSound5.pause();
+        flipSound5.currentTime = 0;
+        flipSound5.play()
     }
     setTimeout(flip5, 7500)
     }
